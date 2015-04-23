415b32c402b8594a23fea2ae8f4752e1607ede69
Michiel Roos <michiel@maxserv.nl>
2015-04-23 16:44:43 +0200

    Add fallback for title if no metadata was found

--- displayFileLinks.php
+++ displayFileLinks.php
@@ -31,63 +31,69 @@ use TYPO3\CMS\Core\Utility\GeneralUtility;
  *    getItemMarkerArray function in tt_news class. see:
  *    EXT:tt_news/pi/class.tx_ttnews.php
  * @param    [type]        $conf: ...
+ *
  * @return    array        the changed markerArray
  */
 function user_displayFileLinks($markerArray, $conf) {
-    $pObj = &$conf['parentObj']; // make a reference to the parent-object
-    $row = $pObj->local_cObj->data;
-    $markerArray['###FILE_LINK###'] = '';
-    $markerArray['###TEXT_FILES###'] = '';
+	$pObj = &$conf['parentObj']; // make a reference to the parent-object
+	$row = $pObj->local_cObj->data;
+	$markerArray['###FILE_LINK###'] = '';
+	$markerArray['###TEXT_FILES###'] = '';
 
-    //load TS config for newsFiles from tt_news
-    $conf_newsFiles = $pObj->conf['newsFiles.'];
-    //Important: unset path
-    $conf_newsFiles['path'] = '';
+	//load TS config for newsFiles from tt_news
+	$conf_newsFiles = $pObj->conf['newsFiles.'];
+	//Important: unset path
+	$conf_newsFiles['path'] = '';
 
-    $local_cObj = GeneralUtility::makeInstance('tslib_cObj');
+	$local_cObj = GeneralUtility::makeInstance('tslib_cObj');
 
-    //workspaces
-    if (isset($row['_ORIG_uid']) && ($row['_ORIG_uid'] > 0)) {
-        // draft workspace
-        $uid = $row['_ORIG_uid'];
-    } else {
-        // live workspace
-        $uid = $row['uid'];
-    }
-    // Check for translation ?
+	//workspaces
+	if (isset($row['_ORIG_uid']) && ($row['_ORIG_uid'] > 0)) {
+		// draft workspace
+		$uid = $row['_ORIG_uid'];
+	} else {
+		// live workspace
+		$uid = $row['uid'];
+	}
+	// Check for translation ?
 
 	/** @var TYPO3\CMS\Core\Resource\FileRepository $fileRepository */
-    $fileRepository = GeneralUtility::makeInstance('TYPO3\\CMS\\Core\\Resource\\FileRepository');
-    $fileObjects = $fileRepository->findByRelation('tt_news', 'tx_falttnews_fal_media', $uid);
+	$fileRepository = GeneralUtility::makeInstance('TYPO3\\CMS\\Core\\Resource\\FileRepository');
+	$fileObjects = $fileRepository->findByRelation('tt_news', 'tx_falttnews_fal_media', $uid);
 
-    if (is_array($fileObjects)) {
-        $files_stdWrap = GeneralUtility::trimExplode('|', $pObj->conf['newsFiles_stdWrap.']['wrap']);
-        $filelinks = '';
-	    /**
-	     * @var \TYPO3\CMS\Core\Resource\FileReference $file
-	     */
-	    foreach ($fileObjects as $key => $file) {
-		    $fileProperties = $file->getOriginalFile()->getProperties();
-		    $referenceProperties = $file->getReferenceProperties();
-		    foreach ($referenceProperties as $key => $value) {
-			    if (in_array($key, array(
-				    'title',
-				    'description',
-				    'downloadname',
-				    'alterative'
-			    ))) {
-				    $fileProperties['reference' . ucfirst($key)] = $value;
-			    }
-		    }
-            $local_cObj->start($fileProperties);
-            $filelinks .= $local_cObj->filelink( $file->getPublicUrl(), $conf_newsFiles);
-        }
+	if (is_array($fileObjects)) {
+		$files_stdWrap = GeneralUtility::trimExplode('|', $pObj->conf['newsFiles_stdWrap.']['wrap']);
+		$filelinks = '';
+		/**
+		 * @var \TYPO3\CMS\Core\Resource\FileReference $file
+		 */
+		foreach ($fileObjects as $key => $file) {
+			$fileProperties = $file->getOriginalFile()->getProperties();
+			$referenceProperties = $file->getReferenceProperties();
+			foreach ($referenceProperties as $key => $value) {
+				if (in_array($key, array(
+					'title',
+					'description',
+					'downloadname',
+					'alterative'
+				))) {
+					$fileProperties['reference' . ucfirst($key)] = $value;
+				}
+			}
 
-        if ($filelinks) {
-            $markerArray['###FILE_LINK###'] = $filelinks . $files_stdWrap[1];
-            $markerArray['###TEXT_FILES###'] = $files_stdWrap[0] . $pObj->local_cObj->stdWrap($pObj->pi_getLL('textFiles'), $pObj->conf['newsFilesHeader_stdWrap.']);
-        }
-    }
-    return $markerArray;
+			// Create fallback title for file if no metadata was found
+			$fileNameParts = pathinfo($fileProperties['name']);
+			$fileProperties['readableName'] = str_replace(array('_'), array(' '), $fileNameParts['filename']);
+
+			$local_cObj->start($fileProperties);
+			$filelinks .= $local_cObj->filelink($file->getPublicUrl(), $conf_newsFiles);
+		}
+
+		if ($filelinks) {
+			$markerArray['###FILE_LINK###'] = $filelinks . $files_stdWrap[1];
+			$markerArray['###TEXT_FILES###'] = $files_stdWrap[0] . $pObj->local_cObj->stdWrap($pObj->pi_getLL('textFiles'), $pObj->conf['newsFilesHeader_stdWrap.']);
+		}
+	}
+
+	return $markerArray;
 }
-
